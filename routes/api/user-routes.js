2652f335f250813df1871c9a5e1f0074ff2ea8f7
Marykate Smith <Mkayy@Marykates-MacBook-Pro.local>
2021-07-10 15:05:00 -0400

    added verify login credentials - not returning that they are logged in in insomnia but does process error statement

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -48,22 +48,26 @@ router.post('/', (req, res) => {
 });
 
 router.post('/login', (req, res) => {
-    // expects {email: 'lernantino@gmail.com', password: 'password1234'}
-    User.findOne({
-        where: {
-            email: req.body.email
-        }
-    }).then(dbUserData => {
-        if (!dbUserData) {
-            res.status(400).json({ message: 'No user with that email address! '});
-            return;
-        }
+  // expects {email: 'lernantino@gmail.com', password: 'password1234'}
+  User.findOne({
+    where: {
+      email: req.body.email
+    }
+  }).then(dbUserData => {
+    if (!dbUserData) {
+      res.status(400).json({ message: 'No user with that email address!' });
+      return;
+    }
 
-        res.json({ user: dbUserData });
+    const validPassword = dbUserData.checkPassword(req.body.password);
 
-        //Verify user
-    });
+    if (!validPassword) {
+      res.status(400).json({ message: 'Incorrect password!' });
+      return;
+    }
 
+    res.json({ user: dbUserData, message: 'You are now logged in!' });
+  });
 });
 
 router.put('/:id', (req, res) => {
@@ -71,7 +75,7 @@ router.put('/:id', (req, res) => {
 
   // pass in req.body instead to only update what's passed through
   User.update(req.body, {
-      individualHooks: true,
+    individualHooks: true,
     where: {
       id: req.params.id
     }
