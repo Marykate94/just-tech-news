5b0fef7eb655f420328c4c02e60a6e798f0d986d
Marykate Smith <Mkayy@Marykates-MacBook-Pro.local>
2021-07-17 13:59:12 -0400

    added cookies

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const exphbs = require('express-handlebars');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-const sequelize = require('./config/connection');
+const sequelize = require('./config/connection.js');
 
 const hbs = exphbs.create({});
 
@@ -17,6 +17,22 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(require('./controllers/'));
+
+const session = require('express-session');
+
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const sess = {
+  secret: 'Super secret secret',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
+
+app.use(session(sess));
 
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
